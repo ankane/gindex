a10bd39dcbe05d0c19e169c733dc18a3a9e2fcbd
Andrew Kane <andrew@chartkick.com>
2019-06-11 04:12:44 -0700

    Cleaned up generator [skip ci]

--- lib/generators/index/index_generator.rb
+++ lib/generators/index/index_generator.rb
@@ -1,22 +1,11 @@
-require "rails/generators/migration"
+require "rails/generators/active_record"
 
 class IndexGenerator < Rails::Generators::Base
-  include Rails::Generators::Migration
-
-  source_root File.expand_path("../templates", __FILE__)
+  include ActiveRecord::Generators::Migration
+  source_root File.join(__dir__, "templates")
 
   argument :table, type: :string
   argument :columns, type: :array
-
-  # Implement the required interface for Rails::Generators::Migration.
-  def self.next_migration_number(dirname) #:nodoc:
-    next_migration_number = current_migration_number(dirname) + 1
-    if ::ActiveRecord::Base.timestamped_migrations
-      [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-    else
-      "%.3d" % next_migration_number
-    end
-  end
 
   def copy_migration
     migrate_path = ActiveRecord::Tasks::DatabaseTasks.migrations_paths.first
