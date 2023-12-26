2bb271907891bca1eba5270e49431fedb7b49d97
Andrew Kane <andrew@ankane.org>
2023-12-26 15:43:01 -0500

    Test with Ruby 3.3 on CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,6 @@ require "minitest/autorun"
 require "minitest/pride"
 
 Combustion.path = "test/internal"
-Combustion.initialize! :active_record
+Combustion.initialize! :active_record do
+  config.load_defaults Rails::VERSION::STRING.to_f
+end
